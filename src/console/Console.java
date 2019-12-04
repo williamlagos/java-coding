c92bdc277c46a5ec2fabee6f9e40b0b2e761a68a
William Lagos <william.lagos@icloud.com>
2019-12-04 16:10:15 -0200

    Adicionado um método de impressão via Console

--- src/console/Console.java
+++ src/console/Console.java
@@ -15,6 +15,8 @@ public class Console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.exit(0);
         // TODO code application logic here
     }
     
